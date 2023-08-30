0f477415769a4ad2d71b814caf5a44b0a71b66a7
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-08-30 17:45:46 +0200

    Started with board class

---
+++ edaa01-workspace/lab1/src/game/Board.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Board {
+	private int totPins;
+	
+	public Board(int nbrOfPins) {
+		totPins = nbrOfPins;
+	}
+	
+	public void setUp() {
+		
+		
+	}
+	public void takePins() {
+		
+	}
+
+}
