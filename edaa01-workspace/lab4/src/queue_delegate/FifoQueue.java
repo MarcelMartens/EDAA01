35e8cf6d6db4f69536ba5019bb8305282c035265
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-11-08 16:44:09 +0100

    .
    .

--- edaa01-workspace/lab4/src/queue_delegate/FifoQueue.java
+++ edaa01-workspace/lab4/src/queue_delegate/FifoQueue.java
@@ -1,59 +1,75 @@
 package queue_delegate;
+
 import java.util.*;
+
+//todo 
+//() impl. metoder
+//() 
 
 public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
 	private Queue<E> list;
 
+	// changed "this."
 	public FifoQueue() {
 		super();
-		list = new LinkedList<E>();
-	}
-	
-	/**	
-	 * Inserts the specified element into this queue, if possible
-	 * post:	The specified element is added to the rear of this queue
-	 * @param	e the element to insert
-	 * @return	true if it was possible to add the element 
-	 * 			to this queue, else false
-	 */
-	public boolean offer(E e) {
-		return false;
+		this.list = new LinkedList<E>();
 	}
 
-	/**	
+	/**
+	 * Inserts the specified element into this queue, if possible
+	 * post: The specified element is added to the rear of this queue
+	 * 
+	 * @param e the element to insert
+	 * @return true if it was possible to add the element
+	 *         to this queue, else false
+	 */
+	// changed +"this.list.offer(e)" -"false"
+	public boolean offer(E e) {
+		return this.list.offer(e);
+	}
+
+	/**
 	 * Returns the number of elements in this queue
+	 * 
 	 * @return the number of elements in this queue
 	 */
-	public int size() {		
-		return 0;
+	// changed +"this.list.size()" -"0"
+	public int size() {
+		return this.list.size();
 	}
-	
-	/**	
-	 * Retrieves, but does not remove, the head of this queue, 
+
+	/**
+	 * Retrieves, but does not remove, the head of this queue,
 	 * returning null if this queue is empty
-	 * @return 	the head element of this queue, or null 
-	 * 			if this queue is empty
+	 * 
+	 * @return the head element of this queue, or null
+	 *         if this queue is empty
 	 */
+	// changed +"this.list.peek()" -"null"
 	public E peek() {
-		return null;
+		return this.list.peek();
 	}
 
-	/**	
-	 * Retrieves and removes the head of this queue, 
+	/**
+	 * Retrieves and removes the head of this queue,
 	 * or null if this queue is empty.
-	 * post:	the head of the queue is removed if it was not empty
-	 * @return 	the head of this queue, or null if the queue is empty 
+	 * post: the head of the queue is removed if it was not empty
+	 * 
+	 * @return the head of this queue, or null if the queue is empty
 	 */
+	// changed +"this.list.poll()"
 	public E poll() {
-		return null;
+		return this.list.poll();
 	}
 
-	/**	
+	/**
 	 * Returns an iterator over the elements in this queue
+	 * 
 	 * @return an iterator over the elements in this queue
-	 */	
+	 */
+	// changed +"this.list.iterator()"
 	public Iterator<E> iterator() {
-		return null;
+		return this.list.iterator();
 	}
 
 }
--- edaa01-workspace/lab4/src/queue_singlelinkedlist/FifoQueue.java
+++ edaa01-workspace/lab4/src/queue_singlelinkedlist/FifoQueue.java
@@ -1,4 +1,5 @@
 package queue_singlelinkedlist;
+
 import java.util.*;
 
 public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
@@ -7,57 +8,84 @@ public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
 
 	public FifoQueue() {
 		super();
-		last = null;
-		size = 0;
+		this.last = null;
+		this.size = 0;
 	}
 
-	/**	
+	/**
 	 * Inserts the specified element into this queue, if possible
-	 * post:	The specified element is added to the rear of this queue
-	 * @param	e the element to insert
-	 * @return	true if it was possible to add the element 
-	 * 			to this queue, else false
+	 * post: The specified element is added to the rear of this queue
+	 * 
+	 * @param e the element to insert
+	 * @return true if it was possible to add the element
+	 *         to this queue, else false
 	 */
 	public boolean offer(E e) {
-		return false;
-	}
-	
-	/**	
-	 * Returns the number of elements in this queue
-	 * @return the number of elements in this queue
-	 */
-	public int size() {		
-		return 0;
-	}
-	
-	/**	
-	 * Retrieves, but does not remove, the head of this queue, 
-	 * returning null if this queue is empty
-	 * @return 	the head element of this queue, or null 
-	 * 			if this queue is empty
-	 */
-	public E peek() {
-		return null;
+		if (this.last == null) {
+			this.last = new QueueNode<E>(e);
+			this.last.next = this.last;
+			this.size++;
+			return true;
+		}
+		try {
+			QueueNode<E> first = this.last.next;
+			this.last.next = new QueueNode<E>(e);
+			this.last = this.last.next;
+			this.size++;
+			return true;
+		} catch (Exception error) {
+			error.printStackTrace();
+			return false;
+		}
 	}
 
-	/**	
-	 * Retrieves and removes the head of this queue, 
+	/**
+	 * Returns the number of elements in this queue
+	 * 
+	 * @return the number of elements in this queue
+	 */
+	public int size() {
+		return this.size;
+	}
+
+	/**
+	 * Retrieves, but does not remove, the head of this queue,
+	 * returning null if this queue is empty
+	 * 
+	 * @return the head element of this queue, or null
+	 *         if this queue is empty
+	 */
+	// change bättre condition?
+	public E peek() {
+		/*
+		 * if (this.last == null){
+		 * return null;
+		 * }
+		 */
+		E ret = this.last == null ? null : this.last.next.element;
+		return ret;
+	}
+
+	/**
+	 * Retrieves and removes the head of this queue,
 	 * or null if this queue is empty.
-	 * post:	the head of the queue is removed if it was not empty
-	 * @return 	the head of this queue, or null if the queue is empty 
+	 * post: the head of the queue is removed if it was not empty
+	 * 
+	 * @return the head of this queue, or null if the queue is empty
 	 */
 	public E poll() {
 		return null;
 	}
-	
-	/**	
+
+	/**
 	 * Returns an iterator over the elements in this queue
+	 * 
 	 * @return an iterator over the elements in this queue
-	 */	
+	 */
 	public Iterator<E> iterator() {
 		return null;
 	}
-	
+
 	private static class QueueNode<E> {
 		E element;
 		QueueNode<E> next;
