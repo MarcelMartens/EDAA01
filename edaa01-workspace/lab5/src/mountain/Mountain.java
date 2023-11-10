103e8cb757aebf1e1821d678d1d16137b26eae2a
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-11-10 18:44:01 +0100

    .

--- edaa01-workspace/lab5/src/mountain/Mountain.java
+++ edaa01-workspace/lab5/src/mountain/Mountain.java
@@ -4,11 +4,17 @@ import fractal.Fractal;
 import fractal.TurtleGraphics;
 
 public class Mountain extends Fractal {
-    private Point[] p0;
+    private Point[] p;
+    private Point p1;
+    private Point p2;
+    private Point p3;
 
-    public Mountain(Point[] startPos) {
+    public Mountain(Point point1, Point point2, Point point3) {
         super();
-        this.p0 = startPos;
+        this.p1 = point1;
+        this.p2 = point2;
+        this.p3 = point3;
+        this.p = new Point[] { point1, point2, point3 };
     }
 
     @Override
@@ -18,9 +24,20 @@ public class Mountain extends Fractal {
     }
 
     @Override
-    public void draw(TurtleGraphics g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    public void draw(TurtleGraphics turtle) {
+        turtle.moveTo(p[0].getX(), p[0].getY());
+        fractalLine(turtle, order, p);
+    }
+
+    private void fractalLine(TurtleGraphics turtle, int order, Point[] p) {
+        if (order == 0) {
+            turtle.forwardTo(p[1].getX(), p[1].getY());
+            turtle.forwardTo(p[2].getX(), p[2].getY());
+            turtle.forwardTo(p[0].getX(), p[0].getY());
+        } else {
+            Point[] halfway = new Point[3];
+            fractalLine(turtle, order - 1, p);
+        }
     }
 
 }
