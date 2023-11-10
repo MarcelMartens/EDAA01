d1fa6e3f97a0a975735416462ad3c74c87aafaef
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-11-10 17:54:22 +0100

    start of lab5

---
+++ edaa01-workspace/lab5/src/mountain/Mountain.java
@@ -0,0 +1,26 @@
+package mountain;
+
+import fractal.Fractal;
+import fractal.TurtleGraphics;
+
+public class Mountain extends Fractal {
+    private Point[] p0;
+
+    public Mountain(Point[] startPos) {
+        super();
+        this.p0 = startPos;
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+    }
+
+    @Override
+    public void draw(TurtleGraphics g) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+}
