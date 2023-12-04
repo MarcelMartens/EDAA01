1527839f23a53025d7bbd2a250d3df60485478f8
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-12-04 18:14:42 +0100

    +methods, tests
    BST: add, size, clear, height, print, constructors
    test: add, size, clear, height, constructors

--- edaa01-workspace/lab6/src/bst/BinarySearchTree.java
+++ edaa01-workspace/lab6/src/bst/BinarySearchTree.java
@@ -3,81 +3,139 @@ package bst;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-
 public class BinarySearchTree<E> {
-  BinaryNode<E> root;  // Används också i BSTVisaulizer
-  int size;            // Används också i BSTVisaulizer
-  private Comparator<E> ccomparator;
-    
+	BinaryNode<E> root; // Används också i BSTVisaulizer
+	int size; // Används också i BSTVisaulizer
+	private Comparator<E> comparator;
+
 	/**
 	 * Constructs an empty binary search tree.
 	 */
 	public BinarySearchTree() {
-		
+		this.size = 0;
+		this.comparator = new Comparator<E>() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public int compare(E o1, E o2) {
+				return ((Comparable<E>) o1).compareTo(o2);
+			}
+		};
 	}
-	
+
 	/**
-	 * Constructs an empty binary search tree, sorted according to the specified comparator.
+	 * Constructs an empty binary search tree, sorted according to the specified
+	 * comparator.
 	 */
 	public BinarySearchTree(Comparator<E> comparator) {
-		
+		this.comparator = comparator;
 	}
 
 	/**
 	 * Inserts the specified element in the tree if no duplicate exists.
-	 * @param x element to be inserted
+	 * 
+	 * @param elem element to be inserted
 	 * @return true if the the element was inserted
 	 */
-	public boolean add(E x) {
+	public boolean add(E elem) {
+		if (root == null) {
+			root = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
+		return add(root, elem);
+	}
+
+	private boolean add(BinaryNode<E> node, E elem) {
+		int comp = comparator.compare(node.element, elem);
+		if (comp > 0) {
+			if (node.left != null) {
+				return add(node.left, elem);
+			}
+			node.left = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
+		if (comp < 0) {
+			if (node.right != null) {
+				return add(node.right, elem);
+			}
+			node.right = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
 		return false;
 	}
-	
+
 	/**
 	 * Computes the height of tree.
+	 * 
 	 * @return the height of the tree
 	 */
 	public int height() {
+		return height(root);
+	}
+
+	private int height(BinaryNode<E> node) {
+		if (node != null) {
+			int r = 0;
+			int l = 0;
+			if (node.right != null)
+				r = height(node.right);
+			if (node.left != null)
+				l = height(node.left);
+			return Math.max(r, l) + 1;
+		}
 		return 0;
 	}
-	
+
 	/**
 	 * Returns the number of elements in this tree.
+	 * 
 	 * @return the number of elements in this tree
 	 */
 	public int size() {
-		return 0;
+		return size;
 	}
-	
+
 	/**
 	 * Removes all of the elements from this list.
 	 */
 	public void clear() {
-		
+		size = 0;
+		root = null;
 	}
-	
+
 	/**
 	 * Print tree contents in inorder.
 	 */
 	public void printTree() {
-
+		printTree(root);
 	}
 
-	/** 
+	private void printTree(BinaryNode<E> node) {
+		if (node != null) {
+			printTree(node.left);
+			System.out.println(node.element);
+			printTree(node.right);
+		}
+	}
+
+	/**
 	 * Builds a complete tree from the elements in the tree.
 	 */
 	public void rebuild() {
 
 	}
-	
+
 	/*
 	 * Adds all elements from the tree rooted at n in inorder to the list sorted.
 	 */
 	private void toArray(BinaryNode<E> n, ArrayList<E> sorted) {
-	
+
 	}
-	
+
 	/*
-	 * Builds a complete tree from the elements from position first to 
+	 * Builds a complete tree from the elements from position first to
 	 * last in the list sorted.
 	 * Elements in the list a are assumed to be in ascending order.
 	 * Returns the root of tree.
@@ -93,7 +151,7 @@ public class BinarySearchTree<E> {
 
 		private BinaryNode(E element) {
 			this.element = element;
-		}	
+		}
 	}
-	
+
 }
---
+++ edaa01-workspace/lab6/src/bst/TestBinarySearchTree1.java
@@ -0,0 +1,100 @@
+package bst;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestBinarySearchTree1 {
+    private BinarySearchTree<Integer> intBst;
+    private BinarySearchTree<String> strBst;
+
+    @BeforeEach
+    public void setUp() {
+        this.intBst = new BinarySearchTree<Integer>((e1, e2) -> (e1).compareTo(e2));
+        this.strBst = new BinarySearchTree<String>((e1, e2) -> (e1).compareTo(e2));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.intBst = null;
+        this.strBst = null;
+    }
+
+    @Test
+    public void testEmpty() {
+        assertEquals("Size not 0", 0, intBst.size());
+        assertEquals("Size not 0", 0, strBst.size());
+        assertNull("Root node not null", intBst.root);
+        assertNull("Root node not null", strBst.root);
+        assertEquals("Height not 0", 0, strBst.height());
+        assertEquals("Height not 0", 0, intBst.height());
+    }
+
+    @Test
+    public void testClear() {
+        addDefault();
+        intBst.clear();
+        strBst.clear();
+        assertEquals("Size not 0", 0, intBst.size());
+        assertEquals("Size not 0", 0, strBst.size());
+        assertNull("Root node not null", intBst.root);
+        assertNull("Root node not null", strBst.root);
+    }
+
+    @Test
+    public void testSize() {
+        addDefault();
+        assertEquals("Size not correct", 5, intBst.size());
+        assertEquals("Size not correct", 5, strBst.size());
+        assertNotNull("Root node is Null", intBst.root);
+        assertNotNull("Root node is Null", strBst.root);
+    }
+
+    @Test
+    public void testHeight() {
+        addDefault();
+        assertEquals("Height not correct", 3, strBst.height());
+        assertEquals("Height not correct", 3, intBst.height());
+    }
+
+    @Test
+    public void testAdd() {
+        intBst.add(7);
+        intBst.add(2);
+        strBst.add("dog");
+        strBst.add("cat");
+        intBst.add(2);
+        strBst.add("cat");
+        assertEquals("Size not correct", 2, intBst.size());
+        assertEquals("Size not correct", 2, strBst.size());
+        assertEquals("Root node is not correct", (Integer) 7, intBst.root.element);
+        assertEquals("Root node is not correct", "dog", strBst.root.element);
+        assertEquals("Left node is not correct", (Integer) 2, intBst.root.left.element);
+        assertEquals("Left node is not correct", "cat", strBst.root.left.element);
+        assertNull("Right node is not Null", intBst.root.right);
+        assertNull("Right node is not Null", intBst.root.right);
+        assertEquals("Height not correct", 2, strBst.height());
+        assertEquals("Height not correct", 2, intBst.height());
+
+    }
+
+    private void addDefault() {
+        intBst.add(7);
+        intBst.add(2);
+        intBst.add(9);
+        intBst.add(8);
+        intBst.add(14);
+        strBst.add("dog");
+        strBst.add("cat");
+        strBst.add("fish");
+        strBst.add("bass");
+        strBst.add("tuna");
+    }
+}
