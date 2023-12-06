f00d5fc1c56247dc13e2a33c12eb11040c61ea63
Marcel <123636617+MarcelMartens@users.noreply.github.com>
2023-12-06 15:59:01 +0100

    Squashed commit of the following:
    commit 67dad12f3138fd66c172d60573bb9598b0f84a09
    Author: Marcel <123636617+MarcelMartens@users.noreply.github.com>
    Date:   Wed Dec 6 15:58:26 2023 +0100
    
        Finished runtimeTest & svar.txt
    
    commit 1776a6d6da7c78f0224aa2252e39e53759eb636d
    Author: Marcel <123636617+MarcelMartens@users.noreply.github.com>
    Date:   Tue Dec 5 13:32:02 2023 +0100
    
        added tests and methods
    
        methods: + toArray, rebuild, buildTree
        tests: + testTime(and connected private methods)
    
    commit 1527839f23a53025d7bbd2a250d3df60485478f8
    Author: Marcel <123636617+MarcelMartens@users.noreply.github.com>
    Date:   Mon Dec 4 18:14:42 2023 +0100
    
        +methods, tests
    
        BST: add, size, clear, height, print, constructors
        test: add, size, clear, height, constructors
    
    commit 658fdd080b159dfb45bd123e4a21d9b629e63c00
    Author: Marcel <123636617+MarcelMartens@users.noreply.github.com>
    Date:   Thu Nov 23 14:42:45 2023 +0100
    
        added svar

--- edaa01-workspace/lab6/src/bst/BinarySearchTree.java
+++ edaa01-workspace/lab6/src/bst/BinarySearchTree.java
@@ -3,87 +3,221 @@ package bst;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+public class BinarySearchTree<E extends Comparable<E>> {
+	BinaryNode<E> root; // Används också i BSTVisaulizer
+	int size; // Används också i BSTVisaulizer
+	private Comparator<E> comparator;
 
-public class BinarySearchTree<E> {
-  BinaryNode<E> root;  // Används också i BSTVisaulizer
-  int size;            // Används också i BSTVisaulizer
-  private Comparator<E> ccomparator;
-    
+	public static void main(String[] args) {
+		getIncementedIntTree(10);
+
+		// BinarySearchTree<Integer> bst = getRandomIntTree(10);
+		BinarySearchTree<String> bst = getDefaultStringTree(10);
+		BinarySearchTree<String> bst2 = getDefaultStringTree(10);
+		BSTVisualizer bv = new BSTVisualizer("Binary Search Tree", 500, 500);
+		bv.drawTree(bst);
+		bst2.rebuild();
+		BSTVisualizer bv2 = new BSTVisualizer("Binary Search Tree", 500, 500);
+		bv2.drawTree(bst2);
+	}
+
+	/**
+	 * 
+	 * @param nbrOfElements number of elements in created tree
+	 * @return a binary search tree with predefined strings
+	 */
+	public static BinarySearchTree<String> getDefaultStringTree(int nbrOfElements) {
+		BinarySearchTree<String> tempBst = new BinarySearchTree<>();
+		String[] strings = new String[] { "cat", "dog", "sun", "moon", "star", "blue", "red", "green", "tree", "bird",
+				"fish", "book", "pen", "cup", "door", "rain", "snow", "cloud", "car", "road", "apple", "orange",
+				"grape", "leaf", "wind", "fire", "water", "rock", "sand", "hill", "river", "lake", "sea", "city",
+				"town", "map", "key", "lock", "ring", "coin", "glass", "wood", "metal", "light", "dark", "cold", "hot",
+				"soft", "hard", "sky", "ball", "ship", "wave", "milk", "rose", "leaf", "cake", "jazz", "kite", "lamp",
+				"desk", "frog", "snow", "rain", "gold", "rice", "bean", "corn", "lime", "pear", "kiwi", "wolf", "bear",
+				"lion", "duck", "swan", "fish", "crab", "shark", "road", "path", "hill", "pond", "park", "shop", "town",
+				"city", "land", "farm", "wood", "rock", "clay", "sand", "dirt", "dust", "rust", "smog", "fog", "leaf",
+				"frog", "mud", "nest", "horn", "moss", "twig", "dew", "seal", "crow", "hawk", "peak", "bark", "ant",
+				"bee", "wasp", "moth", "mite", "slug", "snail", "lamb", "calf", "foal", "hare", "lynx", "wolf", "bull",
+				"goat", "pige", "owl", "lark", "worm", "flea", "silk", "reed", "flax", "hemp", "rice", "maiz", "oats",
+				"rye", "wheat", "spelt", "yarn", "felt", "lace", "silk", "wool", "linen", "flax", "hemp", "cotton",
+				"denim", "twill", "satin", "velvet", "chiffon", "organza", "tulle" };
+		for (String string : strings) {
+			tempBst.add(string);
+			if (tempBst.size >= nbrOfElements)
+				return tempBst;
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param nbrOfElements number of elements in created tree
+	 * @return a binary search tree with random integers
+	 */
+	public static BinarySearchTree<Integer> getRandomIntTree(int nbrOfElements) {
+		BinarySearchTree<Integer> tempBst = new BinarySearchTree<Integer>();
+		while (true) {
+			tempBst.add((int) Math.round(100 * Math.random()));
+			if (tempBst.size >= nbrOfElements)
+				return tempBst;
+		}
+	}
+
+	/**
+	 * 
+	 * @param nbrOfElements number of elements in created tree
+	 * @return a binary search tree with incremented integers
+	 */
+	public static BinarySearchTree<Integer> getIncementedIntTree(int nbrOfElements) {
+		BinarySearchTree<Integer> tempBst = new BinarySearchTree<Integer>();
+		for (int i = 0; i < nbrOfElements; i++) {
+			tempBst.add(i);
+		}
+		return tempBst;
+	}
+
 	/**
 	 * Constructs an empty binary search tree.
 	 */
 	public BinarySearchTree() {
-		
+		this.size = 0;
+		this.comparator = Comparator.naturalOrder();
 	}
-	
+
 	/**
-	 * Constructs an empty binary search tree, sorted according to the specified comparator.
+	 * Constructs an empty binary search tree, sorted according to the specified
+	 * comparator.
 	 */
 	public BinarySearchTree(Comparator<E> comparator) {
-		
+		this.comparator = comparator;
 	}
 
 	/**
 	 * Inserts the specified element in the tree if no duplicate exists.
-	 * @param x element to be inserted
+	 * 
+	 * @param elem element to be inserted
 	 * @return true if the the element was inserted
 	 */
-	public boolean add(E x) {
+	public boolean add(E elem) {
+		if (root == null) {
+			root = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
+		return add(root, elem);
+	}
+
+	private boolean add(BinaryNode<E> node, E elem) {
+		int comp = comparator.compare(node.element, elem);
+		if (comp > 0) {
+			if (node.left != null) {
+				return add(node.left, elem);
+			}
+			node.left = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
+		if (comp < 0) {
+			if (node.right != null) {
+				return add(node.right, elem);
+			}
+			node.right = new BinaryNode<E>(elem);
+			size++;
+			return true;
+		}
 		return false;
 	}
-	
+
 	/**
 	 * Computes the height of tree.
+	 * 
 	 * @return the height of the tree
 	 */
 	public int height() {
+		return height(root);
+	}
+
+	private int height(BinaryNode<E> node) {
+		if (node != null) {
+			int r = 0;
+			int l = 0;
+			if (node.right != null)
+				r = height(node.right);
+			if (node.left != null)
+				l = height(node.left);
+			return Math.max(r, l) + 1;
+		}
 		return 0;
 	}
-	
+
 	/**
 	 * Returns the number of elements in this tree.
+	 * 
 	 * @return the number of elements in this tree
 	 */
 	public int size() {
-		return 0;
+		return size;
 	}
-	
+
 	/**
 	 * Removes all of the elements from this list.
 	 */
 	public void clear() {
-		
+		size = 0;
+		root = null;
 	}
-	
+
 	/**
 	 * Print tree contents in inorder.
 	 */
 	public void printTree() {
-
+		printTree(root);
 	}
 
-	/** 
+	private void printTree(BinaryNode<E> node) {
+		if (node != null) {
+			printTree(node.left);
+			System.out.println(node.element);
+			printTree(node.right);
+		}
+	}
+
+	/**
 	 * Builds a complete tree from the elements in the tree.
 	 */
 	public void rebuild() {
-
+		ArrayList<E> sortedList = new ArrayList<E>();
+		toArray(root, sortedList);
+		root = buildTree(sortedList, 0, sortedList.size() - 1);
 	}
-	
+
 	/*
 	 * Adds all elements from the tree rooted at n in inorder to the list sorted.
 	 */
 	private void toArray(BinaryNode<E> n, ArrayList<E> sorted) {
-	
+		if (n != null) {
+			toArray(n.left, sorted);
+			sorted.add(n.element);
+			toArray(n.right, sorted);
+		}
+
 	}
-	
+
 	/*
-	 * Builds a complete tree from the elements from position first to 
+	 * Builds a complete tree from the elements from position first to
 	 * last in the list sorted.
 	 * Elements in the list a are assumed to be in ascending order.
 	 * Returns the root of tree.
 	 */
 	private BinaryNode<E> buildTree(ArrayList<E> sorted, int first, int last) {
-		return null;
+		if (last < first) {
+			return null;
+		}
+		int mid = (first + last) / 2;
+		BinaryNode<E> newRoot = new BinaryNode<E>(sorted.get(mid));
+		newRoot.left = buildTree(sorted, first, mid - 1);
+		newRoot.right = buildTree(sorted, mid + 1, last);
+		return newRoot;
 	}
 
 	static class BinaryNode<E> {
@@ -93,7 +227,7 @@ public class BinarySearchTree<E> {
 
 		private BinaryNode(E element) {
 			this.element = element;
-		}	
+		}
 	}
-	
+
 }
---
+++ edaa01-workspace/lab6/src/bst/TestBinarySearchTreeComparator.java
@@ -0,0 +1,324 @@
+package bst;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestBinarySearchTreeComparator {
+    private BinarySearchTree<Integer> intBst;
+    private BinarySearchTree<String> strBst;
+
+    @BeforeEach
+    public void setUp() {
+        this.intBst = new BinarySearchTree<Integer>((e1, e2) -> (e1).compareTo(e2));
+        this.strBst = new BinarySearchTree<String>((e1, e2) -> (e1).compareTo(e2));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.intBst = null;
+        this.strBst = null;
+    }
+
+    @Test
+    public void testEmpty() {
+        assertEquals("Size not 0", 0, intBst.size());
+        assertEquals("Size not 0", 0, strBst.size());
+        assertNull("Root node not null", intBst.root);
+        assertNull("Root node not null", strBst.root);
+        assertEquals("Height not 0", 0, strBst.height());
+        assertEquals("Height not 0", 0, intBst.height());
+    }
+
+    @Test
+    public void testClear() {
+        addDefault();
+        intBst.clear();
+        strBst.clear();
+        assertEquals("Size not 0", 0, intBst.size());
+        assertEquals("Size not 0", 0, strBst.size());
+        assertNull("Root node not null", intBst.root);
+        assertNull("Root node not null", strBst.root);
+    }
+
+    @Test
+    public void testSize() {
+        addDefault();
+        assertEquals("Size not correct", 5, intBst.size());
+        assertEquals("Size not correct", 5, strBst.size());
+        assertNotNull("Root node is Null", intBst.root);
+        assertNotNull("Root node is Null", strBst.root);
+    }
+
+    @Test
+    public void testHeight() {
+        addDefault();
+        assertEquals("Height not correct", 3, strBst.height());
+        assertEquals("Height not correct", 3, intBst.height());
+    }
+
+    @Test
+    public void testAdd() {
+        intBst.add(7);
+        intBst.add(2);
+        strBst.add("dog");
+        strBst.add("cat");
+        intBst.add(2);
+        strBst.add("cat");
+        assertEquals("Size not correct", 2, intBst.size());
+        assertEquals("Size not correct", 2, strBst.size());
+        assertEquals("Root node is not correct", (Integer) 7, intBst.root.element);
+        assertEquals("Root node is not correct", "dog", strBst.root.element);
+        assertEquals("Left node is not correct", (Integer) 2, intBst.root.left.element);
+        assertEquals("Left node is not correct", "cat", strBst.root.left.element);
+        assertNull("Right node is not Null", intBst.root.right);
+        assertNull("Right node is not Null", intBst.root.right);
+        assertEquals("Height not correct", 2, strBst.height());
+        assertEquals("Height not correct", 2, intBst.height());
+    }
+
+    /**
+     * tests the speed of BST methods for tree sizes 5, 10, 25, 50 and 100
+     */
+    @Test
+    @Ignore("Disabled because of the time the tests takes")
+    public void testSpeed() {
+        // declaring all time variables and the sizes to be tested
+        String[] varNames = new String[] { "clearTime", "sizeTime", "heightTime", "printTime", "printTimeRebuilt",
+                "rebuildTime", "getTreeTime", "drawTime", "drawTimeRebuilt" };
+        int[] nbrOfElements = { 5, 10, 25, 50, 100 };
+        ArrayList<String> results = new ArrayList<>();
+        // note creates subklass to HashMap. Not optimal, but test is not frequently ran
+        // adds all time variables to a hashmap
+        Map<String, Long> timeMap = new HashMap<>() {
+            {
+                for (String s : varNames) {
+                    put(s, 0L);
+                }
+            }
+        };
+
+        // runs all speed tests for the different tree sizes
+        for (int nbr : nbrOfElements) {
+            for (String varName : varNames) {
+                try {
+                    // Constructs method name and invokes it
+                    Method method = this.getClass().getDeclaredMethod("get" + capitalize(varName), int.class);
+                    long time = (long) method.invoke(this, nbr);
+
+                    // Updates the map
+                    timeMap.put(varName, time);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    fail("Failed to invoke method for variable name: " + varName);
+                }
+            }
+            // appends all results to a string
+            StringBuilder sb = new StringBuilder("\n \n  -   -   -   Runtimes for " + nbr + " elements  -   -   -   ");
+            for (Map.Entry<String, Long> entry : timeMap.entrySet()) {
+                sb.append("\n");
+                sb.append(entry.getKey());
+                sb.append("   -   ");
+                sb.append((double) entry.getValue() / 1000000);
+                sb.append(" ms");
+            }
+            // saves the completed result to an arraylist
+            results.add(sb.toString());
+        }
+        // prints all results from test
+        for (String resultString : results) {
+            System.out.println(resultString);
+        }
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute the get tree commands
+     */
+    @SuppressWarnings("unused")
+    private long getGetTreeTime(int nbr) {
+        long t0 = System.nanoTime();
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        intBst = BinarySearchTree.getIncementedIntTree(nbr);
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 3;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute rebuild
+     */
+    @SuppressWarnings("unused")
+    private long getRebuildTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        long t0 = System.nanoTime();
+        intBst.rebuild();
+        strBst.rebuild();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute print
+     */
+    @SuppressWarnings("unused")
+    private long getPrintTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        long t0 = System.nanoTime();
+        intBst.printTree();
+        strBst.printTree();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute print on rebuilt tree
+     */
+    @SuppressWarnings("unused")
+    private long getPrintTimeRebuilt(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        intBst.rebuild();
+        strBst.rebuild();
+        long t0 = System.nanoTime();
+        intBst.printTree();
+        strBst.printTree();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute draw
+     */
+    @SuppressWarnings("unused")
+    private long getDrawTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        intBst.rebuild();
+        strBst.rebuild();
+        BSTVisualizer bv = new BSTVisualizer("Binary Search Tree", 500, 500);
+        BSTVisualizer bv2 = new BSTVisualizer("Binary Search Tree", 500, 500);
+        long t0 = System.nanoTime();
+        bv.drawTree(intBst);
+        bv2.drawTree(strBst);
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute draw on a rebuilt tree
+     */
+    @SuppressWarnings("unused")
+    private long getDrawTimeRebuilt(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        intBst.rebuild();
+        strBst.rebuild();
+        BSTVisualizer bv = new BSTVisualizer("Binary Search Tree", 500, 500);
+        BSTVisualizer bv2 = new BSTVisualizer("Binary Search Tree", 500, 500);
+        long t0 = System.nanoTime();
+        bv.drawTree(intBst);
+        bv2.drawTree(strBst);
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute clear
+     */
+    @SuppressWarnings("unused")
+    private long getClearTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        long t0 = System.nanoTime();
+        intBst.clear();
+        strBst.clear();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute size
+     */
+    @SuppressWarnings("unused")
+    private long getSizeTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        long t0 = System.nanoTime();
+        intBst.size();
+        strBst.size();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * 
+     * @param nbr number of elements in the tested tree
+     * @return the average time taken to execute height
+     */
+    @SuppressWarnings("unused")
+    private long getHeightTime(int nbr) {
+        intBst = BinarySearchTree.getRandomIntTree(nbr);
+        strBst = BinarySearchTree.getDefaultStringTree(nbr);
+        long t0 = System.nanoTime();
+        intBst.height();
+        strBst.height();
+        long t1 = System.nanoTime();
+        return (t1 - t0) / 2;
+    }
+
+    /**
+     * capitalizes the first character in a string
+     * 
+     * @param str string to be capitalized
+     * @return the capitalized string
+     */
+    private String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+
+    private void addDefault() {
+        intBst.add(7);
+        intBst.add(2);
+        intBst.add(9);
+        intBst.add(8);
+        intBst.add(14);
+        strBst.add("dog");
+        strBst.add("cat");
+        strBst.add("fish");
+        strBst.add("bass");
+        strBst.add("tuna");
+    }
+
+}
